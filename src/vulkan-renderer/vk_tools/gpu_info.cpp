e2fda955c2e5c2f85e2efac0d90063e1a6a25001
Johannes Schneider <johannesschneider@outlook.com>
2021-04-11 13:47:23 +0200

    [gpu-info] Use static_cast instead of implicit conversions

--- src/vulkan-renderer/vk_tools/gpu_info.cpp
+++ src/vulkan-renderer/vk_tools/gpu_info.cpp
@@ -43,19 +43,19 @@ void print_physical_device_queue_families(const VkPhysicalDevice graphics_card) 
         spdlog::debug("Queue count: {}", queue_family_properties[i].queueCount);
         spdlog::debug("Timestamp valid bits: {}", queue_family_properties[i].timestampValidBits);
 
-        if (queue_family_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+        if (static_cast<bool>(queue_family_properties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)) {
             spdlog::debug("VK_QUEUE_GRAPHICS_BIT");
         }
-        if (queue_family_properties[i].queueFlags & VK_QUEUE_COMPUTE_BIT) {
+        if (static_cast<bool>(queue_family_properties[i].queueFlags & VK_QUEUE_COMPUTE_BIT)) {
             spdlog::debug("VK_QUEUE_COMPUTE_BIT");
         }
-        if (queue_family_properties[i].queueFlags & VK_QUEUE_TRANSFER_BIT) {
+        if (static_cast<bool>(queue_family_properties[i].queueFlags & VK_QUEUE_TRANSFER_BIT)) {
             spdlog::debug("VK_QUEUE_TRANSFER_BIT");
         }
-        if (queue_family_properties[i].queueFlags & VK_QUEUE_SPARSE_BINDING_BIT) {
+        if (static_cast<bool>(queue_family_properties[i].queueFlags & VK_QUEUE_SPARSE_BINDING_BIT)) {
             spdlog::debug("VK_QUEUE_SPARSE_BINDING_BIT");
         }
-        if (queue_family_properties[i].queueFlags & VK_QUEUE_PROTECTED_BIT) {
+        if (static_cast<bool>(queue_family_properties[i].queueFlags & VK_QUEUE_PROTECTED_BIT)) {
             spdlog::debug("VK_QUEUE_PROTECTED_BIT");
         }
 
@@ -325,28 +325,28 @@ void print_physical_device_memory_properties(const VkPhysicalDevice graphics_car
 
         const auto &property_flag = gpu_memory_properties.memoryTypes[i].propertyFlags;
 
-        if (property_flag & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT)) {
             spdlog::debug("VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT");
         }
-        if (property_flag & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT)) {
             spdlog::debug("VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT");
         }
-        if (property_flag & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT)) {
             spdlog::debug("VK_MEMORY_PROPERTY_HOST_COHERENT_BIT");
         }
-        if (property_flag & VK_MEMORY_PROPERTY_HOST_CACHED_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_HOST_CACHED_BIT)) {
             spdlog::debug("VK_MEMORY_PROPERTY_HOST_CACHED_BIT");
         }
-        if (property_flag & VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT)) {
             spdlog::debug("VK_MEMORY_PROPERTY_LAZILY_ALLOCATED_BIT");
         }
-        if (property_flag & VK_MEMORY_PROPERTY_PROTECTED_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_PROTECTED_BIT)) {
             spdlog::debug("VK_MEMORY_PROPERTY_PROTECTED_BIT");
         }
-        if (property_flag & VK_MEMORY_PROPERTY_DEVICE_COHERENT_BIT_AMD) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_DEVICE_COHERENT_BIT_AMD)) {
             spdlog::debug("VK_MEMORY_PROPERTY_DEVICE_COHERENT_BIT_AMD");
         }
-        if (property_flag & VK_MEMORY_PROPERTY_DEVICE_UNCACHED_BIT_AMD) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_PROPERTY_DEVICE_UNCACHED_BIT_AMD)) {
             spdlog::debug("VK_MEMORY_PROPERTY_DEVICE_UNCACHED_BIT_AMD");
         }
     }
@@ -356,10 +356,10 @@ void print_physical_device_memory_properties(const VkPhysicalDevice graphics_car
 
         const auto &property_flag = gpu_memory_properties.memoryHeaps[i].flags;
 
-        if (property_flag & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT)) {
             spdlog::debug("VK_MEMORY_HEAP_DEVICE_LOCAL_BIT ");
         }
-        if (property_flag & VK_MEMORY_HEAP_MULTI_INSTANCE_BIT) {
+        if (static_cast<bool>(property_flag & VK_MEMORY_HEAP_MULTI_INSTANCE_BIT)) {
             spdlog::debug("VK_MEMORY_HEAP_MULTI_INSTANCE_BIT");
         }
     }
