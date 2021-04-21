9e23e098e50e64104dcc13c99fdd0a2332d51a57
Johannes Schneider <johannesschneider@outlook.com>
2021-04-21 22:40:59 +0200

    [collision-query] (SQUASH) Apply fail early principle in check_for_collision

--- src/vulkan-renderer/world/collision_query.cpp
+++ src/vulkan-renderer/world/collision_query.cpp
@@ -53,49 +53,55 @@ std::optional<RayCubeCollision<Cube>> OctreeCollisionQuery::check_for_collision(
 
     // First, check if ray collides with bounding sphere.
     // This is much easier to calculate than a collision with a bounding box.
-    if (glm::intersectRaySphere(pos, dir, m_cube.center(), sphere_radius_squared, intersection_distance)) {
-        // Second, check if ray collides with bounding box.
-        // This again is also much faster than to check for collision with every one of the 8 sub cubes.
-        // TODO: This is an axis aligned bounding box! Alignment must account for rotation in the future!
-        if (ray_box_collision(m_cube.bounding_box(), pos, dir)) {
-            if (m_cube.is_leaf()) {
-                // We found a leaf collision. Now we need to determine the selected face,
-                // nearest corner to intersection point and nearest edge to intersection point.
-                return std::make_optional<RayCubeCollision<Cube>>(m_cube, pos, dir);
-            } else {
-                std::size_t hit_candidate_count{0};
-                std::size_t collision_subcube_index{0};
-                float m_nearest_square_distance = std::numeric_limits<float>::max();
-                auto subcubes = m_cube.childs();
+    if (!glm::intersectRaySphere(pos, dir, m_cube.center(), sphere_radius_squared, intersection_distance)) {
+        // No collision found.
+        return std::nullopt;
+    }
 
-                // Iterate through all sub cubes and check for collision.
-                for (std::int32_t i = 0; i < 8; i++) {
-                    if (subcubes[i]->type() != Cube::Type::EMPTY) {
-                        OctreeCollisionQuery subcollision(*subcubes[i]);
-                        if (subcollision.check_for_collision(pos, dir)) {
-                            hit_candidate_count++;
+    // Second, check if ray collides with bounding box.
+    // This again is also much faster than to check for collision with every one of the 8 sub cubes.
+    // TODO: This is an axis aligned bounding box! Alignment must account for rotation in the future!
+    if (!ray_box_collision(m_cube.bounding_box(), pos, dir)) {
+        // No collision found.
+        return std::nullopt;
+    }
 
-                            // If a ray collides with an octant, it can collide with multiple child cubes as it goes
-                            // through it. We need to find the cube which is nearest to the camera and also in front of
-                            // the camera.
-                            const auto distance = subcubes[i]->squared_distance(pos);
+    if (m_cube.is_leaf()) {
+        // We found a leaf collision. Now we need to determine the selected face,
+        // nearest corner to intersection point and nearest edge to intersection point.
+        return std::make_optional<RayCubeCollision<Cube>>(m_cube, pos, dir);
+    } else {
+        std::size_t hit_candidate_count{0};
+        std::size_t collision_subcube_index{0};
+        float m_nearest_square_distance = std::numeric_limits<float>::max();
+        auto subcubes = m_cube.childs();
 
-                            if (distance < m_nearest_square_distance) {
-                                collision_subcube_index = i;
-                                m_nearest_square_distance = distance;
-                            }
-                        }
-                    }
+        // Iterate through all sub cubes and check for collision.
+        for (std::int32_t i = 0; i < 8; i++) {
+            if (subcubes[i]->type() != Cube::Type::EMPTY) {
+                OctreeCollisionQuery subcollision(*subcubes[i]);
+                if (subcollision.check_for_collision(pos, dir)) {
+                    hit_candidate_count++;
 
-                    // If a ray goes through a cube of 8 subcubes, no more than 4 collisions can take place.
-                    if (hit_candidate_count == 4) {
-                        break;
+                    // If a ray collides with an octant, it can collide with multiple child cubes as it goes
+                    // through it. We need to find the cube which is nearest to the camera and also in front of
+                    // the camera.
+                    const auto distance = subcubes[i]->squared_distance(pos);
+
+                    if (distance < m_nearest_square_distance) {
+                        collision_subcube_index = i;
+                        m_nearest_square_distance = distance;
                     }
                 }
+            }
 
-                return std::make_optional<RayCubeCollision<Cube>>(*subcubes[collision_subcube_index], pos, dir);
+            // If a ray goes through a cube of 8 subcubes, no more than 4 collisions can take place.
+            if (hit_candidate_count == 4) {
+                break;
             }
         }
+
+        return std::make_optional<RayCubeCollision<Cube>>(*subcubes[collision_subcube_index], pos, dir);
     }
 
     // No collision found.
